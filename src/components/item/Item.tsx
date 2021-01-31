8214a5a882cb947150a78c9ccbdebbca832c5c0c
Max <maxim.nebela@gmail.com>
2021-01-31 19:18:33 +0200

    css

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import s from "./Item.module.css"
+import clockSvg from "../../media/svg/img/clock.svg";
+import saveSvg from "../../media/svg/img/bookmark.svg";
+//import saveSvgActive from "../../media/svg/img/bookmarkActive.svg";
 
 export const Item:React.FC<{}> = () => {
     return (
@@ -7,7 +10,9 @@ export const Item:React.FC<{}> = () => {
              <div><img className={s.img} src={"https://lh3.googleusercontent.com/proxy/_o25MtfLTupLQ83rbbIFMIXr8CfDDRbAa2e2_NeHcEIZkOm2MhTt5m_y33jDvvo0iA0QlQy8WljrSLRtaBBjH5iJJsdNLLXapr3dGuIluu_lDswRak0N4PI9PHT1cu31A7ebm8M"} alt={"recipe"}/></div>
             <div>
                 <div className={s.title}>BaBaasBBaasBaasaasBaasas</div>
-                <div className={s.time}><b>pizza 15-30pizza 15-30 ()pizza 15-30 ()pizza 15-30 ()pizza 15-30 () ()</b></div>
+                <object type="image/svg+xml" data={saveSvg} className={s.saveSvg}>Save</object>
+                {/*<object type="image/svg+xml" data={saveSvgActive} className={s.saveSvg}>Save</object>*/}
+                <div className={s.time}><b>pizza 15-30</b><object type="image/svg+xml" data={clockSvg} className={s.clockSvg}>Clock</object></div>
                 <div className={s.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda commodi
                     consequuntur deserunt dignissimos dolore doloribus eaque, maiores nam non quasi, ratione
                     suscipit vel, veniam voluptate? Dolorem ex libero voluptatem.</div>

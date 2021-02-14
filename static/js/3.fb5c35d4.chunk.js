92f0f860ecb71158e77414966563bbd52c64bec1
Max <maxim.nebela@gmail.com>
2021-02-14 19:29:25 +0200

    Updates

---
+++ static/js/3.fb5c35d4.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonprecipes=this.webpackJsonprecipes||[]).push([[3],{78:function(t,a,h){"use strict";h.d(a,"a",(function(){return e}));var v=h(0);function e(t){return Object(v.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M18 8h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2V7a6 6 0 1 1 12 0v1zm-2 0V7a4 4 0 1 0-8 0v1h8zm-5 6v2h2v-2h-2zm-4 0v2h2v-2H7zm8 0v2h2v-2h-2z"}}]}]})(t)}}}]);
+//# sourceMappingURL=3.fb5c35d4.chunk.js.map
